68407e713b7d8cf240510fe3e8e223780bac5f66
Jatuphum Tungsilsukchai <43282496+jatu-studiobox@users.noreply.github.com>
2020-07-21 05:57:56 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -43,14 +43,15 @@
 
     function GatheringResult(sdate) {
         try {
-            //const result = await fetch(`https://us-central1-mineko-1.cloudfunctions.net/SearchAndGatheringPsc3YrsLottoPrizeListByDate?sdate=${sdate}`,{mode: 'cors'});
+            const response = await fetch(`https://us-central1-mineko-1.cloudfunctions.net/SearchAndGatheringPsc3YrsLottoPrizeListByDate?sdate=${sdate}`);
+            console.log(response);
             ///const result = await fetch(`https://us-central1-test-exe91.cloudfunctions.net/app/hello`, {mode: 'no-cors'});
-            const url = `https://us-central1-test-exe91.cloudfunctions.net/app/hello`; // site that doesn’t send Access-Control-*
-            fetch(url)
-                .then(response => response.text())
-                .then(contents => console.log(contents))
-                .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
-            console.log(result);
+            // const url = `https://us-central1-test-exe91.cloudfunctions.net/app/hello`; // site that doesn’t send Access-Control-*
+            // fetch(url)
+            //     .then(response => response.text())
+            //     .then(contents => console.log(contents))
+            //     .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+            // console.log(result);
         } catch (error) {
             console.log(error);
         }
