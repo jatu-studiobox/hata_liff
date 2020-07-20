3f5a781b85604d0460370dc47e40424d919dc38f
Jatuphum Tungsilsukchai <43282496+jatu-studiobox@users.noreply.github.com>
2020-07-20 09:33:58 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -41,10 +41,15 @@
         btnLogout.classList.remove('hide');
     }
 
-    async function GatheringResult(sdate) {
+    function GatheringResult(sdate) {
         try {
             //const result = await fetch(`https://us-central1-mineko-1.cloudfunctions.net/SearchAndGatheringPsc3YrsLottoPrizeListByDate?sdate=${sdate}`,{mode: 'cors'});
-			const result = await fetch(`https://us-central1-test-exe91.cloudfunctions.net/app/hello`, {mode: 'no-cors'});
+            ///const result = await fetch(`https://us-central1-test-exe91.cloudfunctions.net/app/hello`, {mode: 'no-cors'});
+            const url = `https://us-central1-test-exe91.cloudfunctions.net/app/hello`; // site that doesn’t send Access-Control-*
+            fetch(url)
+                .then(response => response.text())
+                .then(contents => console.log(contents))
+                .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
             console.log(result);
         } catch (error) {
             console.log(error);
